c54aab3c066bc807e4e4fccf2b379111e1322414
noname nohost <noname.nohost@gmail.com>
2025-05-21 11:12:51 +0200

    script to create lists in md added

---
+++ lists.py
@@ -0,0 +1,10 @@
+import json
+
+
+with open("main.json", "r") as fp:
+    data = json.load(fp)
+
+    keys = list(data.keys())
+    keys.sort()
+    # print(data)
+    for k in keys: print("- [{}](data/{})".format(k, data[k].get("filename", "")))
